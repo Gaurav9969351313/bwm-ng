63ce8277f6f7417e2db5d88b3f498860723cf333
Graham Thomas <thom1229@msu.edu>
2018-07-30 13:59:49 -0700

    Adds manage section for bookings and rentals. Adds delete rental functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
-import { RentalComponent } from './rental/rental.component';
 
 import { RentalModule } from "./rental/rental.module";
 import { AuthModule } from "./auth/auth.module";
+import { ManageModule } from "./manage/manage.module";
 
 const routes: Routes = [
   {path: "", redirectTo: "/rentals", pathMatch: "full"},
@@ -27,6 +27,7 @@ const routes: Routes = [
     BrowserModule,
     RentalModule,
     AuthModule,
+    ManageModule,
     NgbModule.forRoot(),
     ToastModule.forRoot(),
     BrowserAnimationsModule
