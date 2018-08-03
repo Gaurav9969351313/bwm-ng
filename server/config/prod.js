5d4f6bfe0b03b92a7d8a82dc1f207471617aa2fd
Graham Thomas <thom1229@msu.edu>
2018-08-03 11:02:01 -0700

    Fixes amazon key

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   DB_URI: process.env.DB_URI,
   SECRET: process.env.SECRET,
-  AWS_ACCESS_KEY_ID: "AKIAJO7NABSW6PTMEXRA",
-  AWS_SECRET_ACCESS_KEY: "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"
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
 };
